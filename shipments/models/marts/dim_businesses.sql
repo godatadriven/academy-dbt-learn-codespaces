7ac9818b18e68f2200157ef768d18a20e7aca3e3
Manohar Jishu <manohar.jishu@fedex.com>
2024-11-14 13:15:37 +0000

    created .yml's for all the marts models and tested it for uniqueness and not nullable

--- shipments/models/marts/dim_businesses.sql
+++ shipments/models/marts/dim_businesses.sql
@@ -1,5 +1,12 @@
 with businesses as (
     select * from {{ref("base_swift_base_businesses") }}
+),
+create_primary_key as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['business_id','business_name','business_address','business_country'])}} as business_id_sk,
+        *
+    from businesses
 )
 
-select * from businesses
+select * from create_primary_key
+--order by business_id_sk
--- shipments/models/marts/dim_customers.sql
+++ shipments/models/marts/dim_customers.sql
@@ -1,5 +1,11 @@
 with customers as (
     select * from {{ref("base_swift_base_customers") }}
+),
+create_primary_key as (
+    select
+    {{ dbt_utils.generate_surrogate_key(['customer_id','first_name','last_name','city','email'])}} as customer_id_sk,*
+    from customers
 )
 
-select * from customers
+select * from create_primary_key
+order by customer_id_sk
