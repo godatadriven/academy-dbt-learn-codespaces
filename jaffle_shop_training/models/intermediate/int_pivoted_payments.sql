729de562d380ae2cd9fb8757e661990486c0ebe9
Lucy Sheppard <lucy.sheppard@xebia.com>
2024-11-14 07:48:37 +0000

    End of day 2

--- jaffle_shop_training/models/intermediate/int_pivoted_payments.sql
+++ jaffle_shop_training/models/intermediate/int_pivoted_payments.sql
@@ -1,5 +1,5 @@
 -- set the payment_methods (can we use select distinct??)
--- using a for loop for all the sum/case whens
+-- using a for loop for all the sum/case whens 
 
 {%- set payment_methods = dbt_utils.get_column_values(table=ref("stg_stripe__payments"), column='payment_method') %}
 
