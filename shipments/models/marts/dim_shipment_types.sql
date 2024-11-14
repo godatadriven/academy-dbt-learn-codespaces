144bf3f0e8c35be1dde98a2aa3f437e8aa5a26da
Chandan L <chandan.l@fedex.com>
2024-11-14 14:11:26 +0000

    added dim_shipment_types

---
+++ shipments/models/marts/dim_shipment_types.sql
@@ -0,0 +1,18 @@
+{% set nums=[1,2,3,4,5,6,7,8,9,10,11]%}
+{% set names=['january','february','march','april','may','june','july','august','september','october','november']%}
+
+with
+union_stategy_price as (
+    {% for num, name in zip(nums, names) %}
+        select *,
+            {{num}} as {{name}}
+    from {{ ref('stg_swift_transactions__dim_shipment_type_' ~ name) }}
+    {% if not loop.last %}
+    union all
+    {% endif %}
+    {% endfor %}
+ )
+ 
+select
+     *
+     from union_stategy_price
---
+++ shipments/models/staging/swift_transactions/stg_swift_transactions__dim_shipment_type_january.sql
@@ -0,0 +1,20 @@
+with 
+
+source as (
+
+    select * from {{ source('swift_transactions', 'dim_shipment_type_january') }}
+
+),
+
+renamed as (
+
+    select
+        shipment_type,
+        price_per_kg,
+        shipment_type_id
+
+    from source
+
+)
+
+select * from renamed
