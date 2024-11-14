557d7f84086f4bc85fa93b0190f9da8455abb66a
Niels Meijssen <niels.meijssen.osv@fedex.com>
2024-11-14 14:44:06 +0000

    add price strategy test

--- shipments/models/marts/fct_shipments.sql
+++ shipments/models/marts/fct_shipments.sql
@@ -1,5 +1,11 @@
 with shipments as (
     select * from {{ ref('base_swift_transactions_shipments')}}
-)
+    ),
 
-select * from shipments
+    shipment_types as (
+        select * from {{ref('dim_shipment_types')}}
+    )
+
+select s.*, t.price_per_kg from shipments s left join shipment_types t
+    on extract(month from s.label_creation_date) = t.month
+    and s.shipment_type_id = t.shipment_type_id
