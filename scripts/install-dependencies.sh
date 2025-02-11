fb2490d3e006f62747ff667c2d03231889bb03b3
Lucy Sheppard <50021249+lushep@users.noreply.github.com>
2025-02-11 08:49:32 +0100

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,3 +1,4 @@
 pip3 install --user -r requirements.txt
-mkdir -p ~/.dbt/jaffle_shop_training
+mkdir -p ~/.dbt
+cd jaffle_shop_training
 echo "Hi! Start by executing 'dbt init' to create your first dbt project."
