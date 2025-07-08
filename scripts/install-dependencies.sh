eaaaf274d5853c1dc233504017fc3646748bc0ac
Lucy Sheppard <50021249+lushep@users.noreply.github.com>
2025-07-08 05:01:26 +0100

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install OS packages needed for gcloud
-apt-get update && apt-get install -y curl unzip gnupg
-
-# Install gcloud CLI
-echo "Installing gcloud CLI..."
-curl -sSL https://sdk.cloud.google.com | bash
-
-# Add gcloud to PATH for current shell
-export PATH="$HOME/google-cloud-sdk/bin:$PATH"
-
 # Install Python dependencies
 pip3 install --user -r requirements.txt
 
