a7d18cdca6b8e67643e06d735152dd9ac8b94b30
Lucy Sheppard <50021249+lushep@users.noreply.github.com>
2025-07-08 05:02:03 +0100

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Install OS packages needed for gcloud
+apt-get update && apt-get install -y apt-transport-https ca-certificates gnupg curl lsb-release unzip
+
 # Install Python dependencies
 pip3 install --user -r requirements.txt
 
