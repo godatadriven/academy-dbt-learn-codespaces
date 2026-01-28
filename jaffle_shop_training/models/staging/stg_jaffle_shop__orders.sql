e946abf696267674e905e249bab90543778db161
Tim Tegel <tim.tegel@persgroep.net>
2026-01-28 12:19:16 +0000

    training

---
+++ jaffle_shop_training/models/staging/stg_jaffle_shop__orders.sql
@@ -0,0 +1,20 @@
+with source as 
+    (
+    select  id,
+            user_id,
+            order_date,
+            status
+    from   {{ source('jaffle_shop', 'orders') }}
+    ),
+
+renamed as
+    (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from source
+    )
+
+select * from renamed
