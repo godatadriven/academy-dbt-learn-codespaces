048a699078f7a20a719ddf2ed389006f39ea56be
Mieke De Maere <mieke.de.maere@persgroep.net>
2026-02-19 12:18:16 +0000

    homework done

---
+++ jaffle_shop_training/tests/generic/test_is_even_to_column_ge0.sql
@@ -0,0 +1,26 @@
+{% test is_even(model, column_name) %}
+
+with validation as (
+
+    select
+        {{ column_name }} as even_field
+
+    from {{ model }}
+
+),
+
+validation_errors as (
+
+    select
+        even_field
+
+    from validation
+    -- if this is true, then even_field is actually odd!
+    where even_field < 0 or even_field = 0
+
+)
+
+select *
+from validation_errors
+
+{% endtest %}
