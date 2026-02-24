ed6b160c88f3bd954b9e550982abbd632bff63e7
Eva Liekens <eva.liekens@persgroep.net>
2026-02-24 10:49:24 +0000

    fix syntax error in dim_customers

--- jaffle_shop_training/models/marts/dim_customers.sql
+++ jaffle_shop_training/models/marts/dim_customers.sql
@@ -29,7 +29,7 @@ customer_orders as (
         min(orders.order_date)  as first_order_date,
         max(orders.order_date)  as most_recent_order_date,
         count(orders.order_id)  as number_of_orders,
-        sum(order_amount)       as customer_lifetimevalue
+        sum(orders.order_amount)       as customer_lifetimevalue
 
     from orders
 
@@ -46,7 +46,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.customer_lifetimevalue
 
     from customers
