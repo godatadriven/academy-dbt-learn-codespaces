463cdc7e27c085dbd1dce4bd4f45c86cb7566a29
Elisa Roelants <elisa.roelants@persgroep.net>
2026-02-25 10:55:51 +0000

    oefeningen

---
+++ jaffle_shop_training/models/intermediate/int__pivoted_payments.sql
@@ -0,0 +1,21 @@
+-- get rid of last comma
+-- get rid of unnecessary whitespace
+-- set the list at the top of the model
+-- pull the values of payment_method from the column in the table
+
+{%- set payment_methods = dbt_utils.get_column_values(
+    table=ref('stg_stripe__payments'), 
+    column='payment_method') 
+    -%}
+--{% set payment_methods = ['credit_card','bank_transfer','gift_card','coupon'] %}
+
+select 
+    order_id,
+{% for payment_method in payment_methods %}
+    {%- if loop.first is false -%}
+    ,
+    {%- endif -%}
+    sum (case when payment_method = '{{payment_method}}' then payment_amount else 0 end) as {{payment_method}}_amount
+{% endfor %}
+from {{ ref ('stg_stripe__payments')}}
+group by all
