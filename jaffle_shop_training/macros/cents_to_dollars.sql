9f0bca5373b965e5084a05324860a6f3c62b1d76
Lucy Sheppard <lucy.sheppard@xebia.com>
2026-07-08 07:07:24 +0000

    start of day 2

--- jaffle_shop_training/macros/cents_to_dollars.sql
+++
@@ -1,3 +0,0 @@
-{% macro cents_to_dollars(column, scale=2) %}
-{{ column }}/100::numeric(16, {{ scale }})
-{% endmacro %}
--- jaffle_shop_training/models/staging/stripe/stg_stripe__payments.sql
+++ jaffle_shop_training/models/staging/stripe/stg_stripe__payments.sql
@@ -7,7 +7,7 @@ renamed as (
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-        {{ cents_to_dollars('amount') }} as payment_amount,
+        amount/100 as payment_amount,
         created as created_at,
         _batched_at as _batched_at
 
